e34a35a768d5e8179b3775ceca88bb9a2d65779d
harshit <Harshitpokharna24@gmail.com>
2026-01-25 15:01:21 +0530

    done

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosError } from 'axios';
 
 // API configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Create axios instance
 export const apiClient: AxiosInstance = axios.create({
